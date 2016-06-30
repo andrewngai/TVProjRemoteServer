b28b7d971ca0d67e6c21863017602759a774eb24
andrewngai <andrewngai>
2016-06-30 16:21:11 -0700

    Added SHARP ON(sendCommand) button and javascript function

--- static/dashboard.js
+++ static/dashboard.js
@@ -5,6 +5,7 @@ Javascript file for dashboard.html
 
 var deviceCommands;
 var xhr;
+
 function startupScript(){
     fetchDeviceCommands();
 }
@@ -24,6 +25,21 @@ function fetchDeviceCommands(){
                 alert("Device Commands Fetch Failed");
             }
     }, false);
+}
 
+function sendCommand(device, command){
+    var xhr = new XMLHttpRequest();
+    var url = "sendCommand?device=" + device + "&command=" + command;
+    xhr.open("GET",url, true);
+    xhr.send();
 
+    xhr.addEventListener("readystatechange",
+        function (){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                var response = xhr.responseText;
+                document.getElementById("text").innerHTML = response;
+            }else if(xhr.readyState == 4 && (xhr.status == 500 || xhr.status == 404)){
+                alert("Commands Send Failed");
+            }
+    }, false);
 }
