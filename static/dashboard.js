cb7c55542812451f16f14b22b96d60275f39fbb5
andrewngai <andrew.ngai215@gmail.com>
2016-07-15 16:49:51 -0700

    Implemented applyPreset

--- static/dashboard.js
+++ static/dashboard.js
@@ -43,3 +43,20 @@ function sendCommand(device, command, channel){
             }
     }, false);
 }
+
+function applyPreset(congregation){
+    var xhr = new XMLHttpRequest();
+    var url = "applyPreset?congregation=" + congregation;
+    xhr.open("GET",url, true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange",
+        function (){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                var response = xhr.responseText;
+                document.getElementById("text").innerHTML = response;
+            }else if(xhr.readyState == 4 && (xhr.status == 500 || xhr.status == 404)){
+                alert("Apply Preset Failed");
+            }
+    }, false);
+}
